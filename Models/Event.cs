beb58ff0916e216c3967d98a35b5cf2a630db36c
Sondre071 <Sirciller@gmail.com>
2025-02-26 18:24:58 +0100

    Initial commit!

---
+++ Models/Event.cs
@@ -0,0 +1,19 @@
+﻿using System.Runtime.InteropServices.JavaScript;
+
+namespace EventEaseProject.Models
+{
+    public class Event
+    {
+
+        public string Name { get; set; }
+        public string Date { get; set; }
+        public string Location { get; set; }
+        
+        public Event(string name, string date, string location)
+        {
+            Name = name;
+            Date = date;
+            Location = location;
+        }
+    }
+}
